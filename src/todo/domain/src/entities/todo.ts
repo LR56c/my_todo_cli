ac2db97c263b8f6b793a9a41a03032c181fa87cf
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-04-13 01:03:52 -0400

    fix(todo.domain): add entities

--- src/todo/domain/src/entities/todo.ts
+++ src/todo/domain/src/entities/todo.ts
@@ -1,32 +1,18 @@
-// import {z} from "zod";
-// export const TodoSchema = z
-//     .object({
-//         id: z.number().positive(),
-//         title: z.string().max(5),
-//         createdAt: z.date(),
-//         updatedAt: z.date(),
-//         completed: z.boolean(),
-//     })
-//     .required();
-//
-// export type Todo = z.infer<typeof TodoSchema>;
-
 import {TodoId} from "./todo_id";
 import {TodoTitle} from "./todo_title";
 import {TodoCompleted} from "./todo_completed";
-import {CreatedAt} from "todo-shared/src/value_objects/created_at";
-import {UpdatedAt} from "todo-shared/src/value_objects/updated_at";
+import {CreatedAt} from "app-shared/src/value_objects/created_at";
+import {UpdatedAt} from "app-shared/src/value_objects/updated_at";
 
 export class Todo {
-  private constructor(private todoId: TodoId,
-                      private todoTitle: TodoTitle,
-                      private todoCompleted: TodoCompleted,
-                      private createdAt: CreatedAt,
-                      private updatedAt: UpdatedAt
-  ) {
-  }
+  private constructor(public readonly todoId: TodoId,
+                      public readonly todoTitle: TodoTitle,
+                      public readonly todoCompleted: TodoCompleted,
+                      public readonly createdAt: CreatedAt,
+                      public readonly updatedAt: UpdatedAt
+  ) {}
 
-  static create(todoId: TodoId,
+  public static create(todoId: TodoId,
                 todoTitle: TodoTitle,
                 todoCompleted: TodoCompleted,
                 createdAt: CreatedAt,
--- src/todo/domain/src/repository/todo_repository.ts
+++ src/todo/domain/src/repository/todo_repository.ts
@@ -1,14 +1,15 @@
 import {Todo} from "../entities/todo";
 import {Result} from "oxide.ts";
+import {TodoId} from "../entities/todo_id";
 
 export interface TodoRepository {
-    createTodo(newTodo: Todo): Promise<Result<boolean, Error>>
+  createTodo(newTodo: Todo): Promise<Result<boolean, Error>>;
 
-    deleteTodo(id: number): Promise<Result<boolean, Error>>
+  deleteTodo(id: TodoId): Promise<Result<boolean, Error>>;
 
-    getTodoById(id: number): Promise<Result<Todo, Error>>
+  getTodoById(id: TodoId): Promise<Result<Todo, Error>>;
 
-    getTodos(): Promise<Result<Todo[], Error>>
+  getTodos(): Promise<Result<Todo[], Error>>;
 
-    updateTodo(newTodo: Todo): Promise<Result<boolean, Error>>
+  updateTodo(newTodo: Todo): Promise<Result<boolean, Error>>;
 }
